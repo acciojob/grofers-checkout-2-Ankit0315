8f45e537968f6453e3c43b970638056456e33067
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-28 10:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-
-const prices = document.querySelectorAll('[data-ns-test="price"]');
-
+let tableEl = document.getElementById('myTable');
+let priceArr = Array.from(document.querySelectorAll("td[data-ns-test]"));
 
 let totalPrice = 0;
-for(let i=0; i<prices.length; i++)
-	{
-		totalPrice+=parseInt(prices[i].innerText);
-	}
-const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
-grandTotal.innerText = totalPrice.toString();
+for (const priceOfEachItem of priceArr) {
+  totalPrice += Number(priceOfEachItem.textContent);
+}
+
+let totalRow = document.createElement('tr');
+let newcell = totalRow.insertCell(0);
+newcell.textContent = totalPrice
+totalRow.setAttribute('data-ns-test', 'grandTotal')
+
+tableEl.insertAdjacentElement('beforeend', totalRow);
