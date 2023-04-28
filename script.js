bba90725702242200c0c68e5f32f893d307eef78
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-28 10:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+
+
+let totalPrice = 0;
+prices.forEach(price => {
+  totalPrice += parseFloat(price.innerText);
+});
+
+const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotal.innerText = totalPrice.toString();
